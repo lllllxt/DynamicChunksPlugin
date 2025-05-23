0a8443b44d8c73ae6f070a78f4bd7d40c569e7c4
lllllxt <lllllxt@qq.com>
2025-05-23 23:18:31 +0800

    init

---
+++ DynamicChunksPlugin.js
@@ -0,0 +1,45 @@
+/**
+ * html-webpack-plugin@3.x版本，动态注入资源
+ * 据说html-webpack-plugin@4 @5 已经修复了，那就不要用这个plugin了
+ * 
+ * 技术背景：
+ *   @vue/cli-service@4.x中，
+ *   在vue-cli配置多页面（pages)，且自定义的splitChunks,splitChunks中有的包被分割了，分割后如index~123.111.js,index~123.222.js。
+ *   而html-webpack-plugin@3.x中，是判断 pages.chunks.indexOf("index~123")===-1 以过滤需要注入的chunk名。
+ * 问题：
+ *   由于index~123中“123”是动态不确定的，故无法在pages中配置准确chunks，导致生产构建后无法想html注入script标签。
+ * 解决：
+ *   据说升级html-webpack-plugin到4或5可解决这个问题(实测更多屁事)，
+ *   况且@vue/cli-service@4.x 强依赖了html-webpack-plugin@3.x，即使单独升级html-webpack-plugin到更高版本也没用。
+ *   
+ *   也可以升级@vue/cli 到5就没这些屁事了，
+ *   但奈何项目较大，升级成本比较高 也无法预估风险，故写下此插件
+ * 
+ */
+class DynamicChunksPlugin {
+  apply(compiler) {
+    // 注册compilation钩子HtmlWebpackPlugin 3
+    compiler.hooks.compilation.tap('DynamicChunksPlugin', compilation => {
+      compilation.hooks.htmlWebpackPluginBeforeHtmlGeneration.tapAsync('DynamicChunksPlugin', (data, cb) => {
+        const allowChunks = plugin.options.chunks // vue.config.js 中pages配置的
+
+        const plugin = data.plugin // HtmlWebpackPlugin实例
+        const allChunks = compilation.getStats().toJson().chunks
+        // 过滤需要的chunks
+        let chunks = allChunks.filter(chunk => chunk.names.some(name => allowChunks.some(v => name.includes(v))))
+        // 获取到splitChunk切包后的chunks后，调用HtmlWebpackPlugin实例内部方法排序
+        // Sort chunks
+        chunks = plugin.sortChunks(chunks, plugin.options.chunksSortMode, compilation)
+        // 调用HtmlWebpackPlugin实例内部方法获取静态资源配置
+        // Get assets
+        const assets = plugin.htmlWebpackPluginAssets(compilation, chunks)
+        // Merge assets 直接data.assets={} 不会作用到后续生命周期钩子中
+        Object.assign(data.assets, assets)
+
+        cb(null, data)
+      })
+    })
+  }
+}
+
+module.export = DynamicChunksPlugin
--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# DynamicChunksPlugin
-hack：@vue/cli@4.x下，配置多页面和splitChunk导致静态资源注入失败问题hack
+# DynamicChunksPlugin 动态注入包
+> 仅适用于 html-webpack-plugin@3.x
+
+> hack：@vue/cli@4.x下，配置多页面和splitChunk导致静态资源注入失败问题
+
+
+
+
+# 技术背景
+  `@vue/cli-service@4.x`下，配置了多页面（pages)，且自定义了splitChunks，splitChunks中有的包被分割了，分割后如`index~123.111.js`，`index~123.222.js`。
+  而`html-webpack-plugin@3.x`中，是判断 `pages.chunks.indexOf("index~123")===-1` 以过滤需要注入的chunk名。
+
+# 问题
+  由于`index~123`中`123`是动态不确定的，故无法在pages中配置准确chunks，导致生产构建后无法想html注入script标签。
+
+# 解决方案
+  1. 据说升级html-webpack-plugin到4或5可解决这个问题（实测更多屁事，根本解决不完），
+  况且@vue/cli-service@4.x 强依赖了html-webpack-plugin@3.x，即使单独升级html-webpack-plugin到更高版本也没用
+  2. 升级@vue/cli 到5就没这些屁事了（没实测，手上项目较大，升级成本比较高 也无法预估风险）
+  3. 用我写这个Plugin，完美解决
+
+# 使用方法
+
+受众不多，就不发布npm了，自行copy使用吧
+```
+<!-- vue.config.js -->
+import DynamicChunksPlugin from './plugin/DynamicChunksPlugin'
+
+const IS_DEV = process.env.NODE_ENV === 'development'
+// way1
+...
+plugins: [...(IS_DEV?[new DynamicChunksPlugin()]:[])]
+...
+
+// way2
+...
+chainWebpack: config => {
+  ...
+  config.when(!IS_DEV, config=>{
+    config.plugin('dynamic-chunks-plugin').use(DynamicChunksPlugin)
+  })
+  ...
+}
+...
+
+
+```
+
+# 参考资料
+https://juejin.cn/post/7278286913945387068
+
+https://blog.csdn.net/suckshit/article/details/122897416
+
+https://github.com/vuejs/vue-cli/issues/4944
