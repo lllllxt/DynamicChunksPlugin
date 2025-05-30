bb4a9b35fc1d6a4682b24b95c97a8813b95355ee
lllllxt <lllllxt@qq.com>
2025-05-30 17:08:13 +0800

    删除冗余注释

--- DynamicChunksPlugin.js
+++ DynamicChunksPlugin.js
@@ -1,19 +1,8 @@
 /**
- * html-webpack-plugin@3.x版本，动态注入资源
- * 据说html-webpack-plugin@4 @5 已经修复了，那就不要用这个plugin了
+ * hack：html-webpack-plugin@3.x版本动态注入资源bug
  * 
- * 技术背景：
- *   @vue/cli-service@4.x中，
- *   在vue-cli配置多页面（pages)，且自定义的splitChunks,splitChunks中有的包被分割了，分割后如index~123.111.js,index~123.222.js。
- *   而html-webpack-plugin@3.x中，是判断 includedChunks.indexOf(chunkName)===-1 以过滤需要注入的chunk名。
- * 问题：
- *   由于index~123中“123”是动态，在配置pages时无法获取splitChunks后的动态chunkName，导致构建后无法向html注入script标签。
- * 解决：
- *   据说升级html-webpack-plugin到4或5可解决这个问题(实测更多屁事)，
- *   况且@vue/cli-service@4.x 强依赖了html-webpack-plugin@3.x，即使单独升级html-webpack-plugin到更高版本也没用。
- *   
- *   也可以升级@vue/cli 到5就没这些屁事了，
- *   但奈何项目较大，升级成本比较高 也无法预估风险，故写下此插件
+ * @author lllllxt
+ * @see https://github.com/lllllxt/DynamicChunksPlugin
  * 
  */
 class DynamicChunksPlugin {
